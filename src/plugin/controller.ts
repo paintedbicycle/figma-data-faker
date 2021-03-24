90d169505082d76e5f9ce48a7318016ecc0b3114
Paul Wright <paul@paintedbicycle.com>
2021-03-23 20:47:29 -0400

    Bugfix: Handle case where text has multiple styles

--- src/plugin/controller.ts
+++ src/plugin/controller.ts
@@ -1,35 +1,47 @@
-import find from "lodash/find";
-import data from "../app/assets/data";
+import find from 'lodash/find';
+import data from '../app/assets/data';
 
 figma.showUI(__html__);
 
 figma.ui.onmessage = async (msg) => {
-  if (msg.type === "replace-text") {
-    // Loop through all selected nodes
-    for (const node of figma.currentPage.selection) {
-      // Get out of here if it's not text
-      if (node.type !== "TEXT") return;
+    if (msg.type === 'replace-text') {
+        // Loop through all selected nodes
+        for (const node of figma.currentPage.selection) {
+            // Get out of here if it's not text
+            if (node.type !== 'TEXT') return;
 
-      // We must re-load the font for a text layer for some Figma reason
-      // @ts-ignore
-      await figma.loadFontAsync(node.fontName);
+            // We must re-load the font for a text layer for some Figma reason
+            // Case 1: If the layer has multiple text styles
+            // @ts-ignore
+            if (typeof node.fontName === 'symbol') {
+                const len = node.characters.length;
+                for (let i = 0; i < len; i++) {
+                    // @ts-ignore
+                    await figma.loadFontAsync(node.getRangeFontName(i, i + 1));
+                }
 
-      // Grab the data for this specific pieces of data
-      const data = lookupData(msg.identifier);
+            // Case 2: If only one style
+            } else {
+                //@ts-ignore
+                await figma.loadFontAsync(node.fontName);
+            }
 
-      // Run the function that comes from the look up
-      const replacement = data.replace();
+            // Grab the data for this specific pieces of data
+            const data = lookupData(msg.identifier);
 
-      // Finally set the text
-      node.characters = replacement;
+            // Run the function that comes from the look up
+            const replacement = data.replace();
+
+            // Finally set the text
+            node.characters = replacement;
+        }
     }
-  }
 };
 
 function lookupData(identifier) {
-  // Pull all information from the availableGaps object
-  return find(data, (object) => {
-    // Pick the gap object that corresponds to this form
-    return object.identifier === identifier;
-  });
+    // Pull all information from the availableGaps object
+    return find(data, (object) => {
+        // Pick the gap object that corresponds to this form
+        return object.identifier === identifier;
+    });
 }
